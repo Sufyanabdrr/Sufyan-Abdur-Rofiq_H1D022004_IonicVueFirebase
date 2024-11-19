1c0af10922b9ce74e82948ac4c3adbabbd643985
Sufyanabdrr <sufyanabdur123@gmail.com>
2024-11-19 16:23:29 +0700

    first commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzj-hvCjF9HOiquQHEv1KoNwn5OXYB3Bo",
+    authDomain: "vue-firebase-6c70d.firebaseapp.com",
+    projectId: "vue-firebase-6c70d",
+    storageBucket: "vue-firebase-6c70d.firebasestorage.app",
+    messagingSenderId: "625768184739",
+    appId: "1:625768184739:web:4c636c76c987506ac27816"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
